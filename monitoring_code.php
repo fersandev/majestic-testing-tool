203ccef693733da4aeb57e4cd4bc963b6940e215
fernando <fernando.sanchez@majesticmedia.ca>
2017-05-08 11:24:22 -0400

    monitoring code changed

--- monitoring_code.php
+++ monitoring_code.php
@@ -21,6 +21,7 @@ if($unitMonitor['status'] == 'ok') {
 	if($unitMonitorInfo['implementingType'] == 'php') {
 		echo('<div><p>Monitoring PHP code to be inyected in functionality that require to be monitored</p>');
 		echo('
+// Unit Monitor
 $resultToTest = \'RESULT TO CHECK\';
 $curl = curl_init();curl_setopt_array($curl, array(CURLOPT_URL => $_SERVER[\'SERVER_NAME\']."/vendor/fersandev/majestic-testing-tool/mttphp.php?flag=true&keyword=".\''.urlencode($unitMonitorInfo['keyword']).'\'."&pathFile=".urlencode($_SERVER[\'REQUEST_URI\'])."&resultToTest=".urlencode($resultToTest),
   CURLOPT_RETURNTRANSFER => true,CURLOPT_ENCODING => "",CURLOPT_MAXREDIRS => 10,CURLOPT_TIMEOUT => 30,CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,CURLOPT_CUSTOMREQUEST => "GET",CURLOPT_HTTPHEADER => array("cache-control: no-cache"),));$response = curl_exec($curl);curl_close($curl);
@@ -34,6 +35,7 @@ $curl = curl_init();curl_setopt_array($curl, array(CURLOPT_URL => $_SERVER[\'SER
 		echo('
 			<xmp>
 <script>
+// Unit Monitor
 		var resultToTest = "RESULT TO CHECK";
 		var xhttp = new XMLHttpRequest();xhttp.onreadystatechange = function() {if (this.readyState == 4 && this.status == 200) {var response = this.responseText;}};xhttp.open("GET", "http://<?= $_SERVER[\'SERVER_NAME\'] ?>/vendor/fersandev/majestic-testing-tool/mttphp.php?flag=true&resultToTest="+resultToTest+"&keyword=<?= \''.urlencode($unitMonitorInfo['keyword']).'\' ?>&pathFile=<?= urlencode($_SERVER[\'REQUEST_URI\']) ?>", true);xhttp.send();
 </script>
