3f7069e66825213e3a3d33be51249751dd0ae970
fernando <fernando.sanchez@majesticmedia.ca>
2017-05-10 16:19:16 -0400

    result to encode added

--- monitoring_code.php
+++ monitoring_code.php
@@ -19,25 +19,35 @@ if($unitMonitor['status'] == 'ok') {
 	$unitMonitorInfo = $unitMonitor['result'];
 
 	if($unitMonitorInfo['implementingType'] == 'php') {
+		$resultToTestCodePortion = 'urlencode($resultToTest)';
+		if($unitMonitorInfo['typeValueExpected'] == 'boolean') {
+			$resultToTestCodePortion = '$resultToTest';
+		}
+
 		echo('<div><p>Monitoring PHP code to be inyected in functionality that require to be monitored</p>');
 		echo('
 // Unit Monitor<br>
 $resultToTest = \'RESULT TO CHECK\'; 
 <br>
-$curl = curl_init();curl_setopt_array($curl, array(CURLOPT_URL => $_SERVER[\'SERVER_NAME\']."/vendor/fersandev/majestic-testing-tool/mttphp.php?flag=true&json=".json_encode(array(\'keyword\'=>\''.$unitMonitorInfo['keyword'].'\',\'pathFile\'=>$_SERVER[\'REQUEST_URI\'],\'resultToTest\'=>urlencode($resultToTest))), CURLOPT_RETURNTRANSFER => true,CURLOPT_ENCODING => "",CURLOPT_MAXREDIRS => 10,CURLOPT_TIMEOUT => 30,CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,CURLOPT_CUSTOMREQUEST => "GET",CURLOPT_HTTPHEADER => array("cache-control: no-cache"),));$response = curl_exec($curl);curl_close($curl);
+$curl = curl_init();curl_setopt_array($curl, array(CURLOPT_URL => $_SERVER[\'SERVER_NAME\']."/vendor/fersandev/majestic-testing-tool/mttphp.php?flag=true&json=".json_encode(array(\'keyword\'=>\''.$unitMonitorInfo['keyword'].'\',\'pathFile\'=>$_SERVER[\'REQUEST_URI\'],\'resultToTest\'=>'.$resultToTestCodePortion.')), CURLOPT_RETURNTRANSFER => true,CURLOPT_ENCODING => "",CURLOPT_MAXREDIRS => 10,CURLOPT_TIMEOUT => 30,CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,CURLOPT_CUSTOMREQUEST => "GET",CURLOPT_HTTPHEADER => array("cache-control: no-cache"),));$response = curl_exec($curl);curl_close($curl);
 			');
 		echo('</div>');		
 	}
 
 
 	if($unitMonitorInfo['implementingType'] == 'js') {
+		$resultToTestCodePortion = 'encodeURIComponent(resultToTest)';
+		if($unitMonitorInfo['typeValueExpected'] == 'boolean') {
+			$resultToTestCodePortion = 'resultToTest';
+		}
+
 		echo('<div><p>Monitoring JavaScript code to be inyected in functionality that require to be monitored</p>');
 		echo('
 			<xmp>
 <script>
 // Unit Monitor
 			var resultToTest = "RESULT TO CHECK";
-			var json = {keyword:"'.$unitMonitorInfo['keyword'].'", pathFile:"<?= $_SERVER[\'REQUEST_URI\'] ?>", resultToTest:encodeURIComponent(resultToTest)};
+			var json = {keyword:"'.$unitMonitorInfo['keyword'].'", pathFile:"<?= $_SERVER[\'REQUEST_URI\'] ?>", resultToTest:'.$resultToTestCodePortion.'};
 		var xhttp = new XMLHttpRequest();xhttp.onreadystatechange = function() {if (this.readyState == 4 && this.status == 200) {var response = this.responseText;}};var serverName="<?= $_SERVER[\'SERVER_NAME\'] ?>";xhttp.open("GET", "http://"+serverName+"/vendor/fersandev/majestic-testing-tool/mttphp.php?flag=true&json="+JSON.stringify(json)+"");xhttp.send();
 </script>
 			</xmp>
